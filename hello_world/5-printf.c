80e1a049b75e9c6ea6a75b1d4fb74d49b67cc7a2
ananas122 <melle.noraa@gmail.com>
2023-02-17 08:07:48 -0800

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+
+
+/**
+*main - Entry point
+*
+*Return Always 0 (succes)
+*/
+
+int main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+retourn (0);
+  
+}    
