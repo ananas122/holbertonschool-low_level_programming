cffb5047fdfa7bb1719935bc1960d158e500b0d1
ananas122 <melle.noraa@gmail.com>
2023-02-20 05:14:13 -0800

    ajout d espace

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+/*
+ * main - Entry point 
+ *Return Alawyas 0 (success)
+ */
+int main(void)
+
+{ int n;
+
+  srand(time(0))
+    n = rand () -RAND_MAX / 2;
+
+  if (n % 5 > 10)
+    printf("last digit of %d is %d and is greater than 5\n")
+
+    else (n == 
+
+
+	  
+
+  return (0) ;
+
+}
