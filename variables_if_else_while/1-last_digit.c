29b2ea0b96c9434b071b48ddbb3b4adfef529470
ananas122 <melle.noraa@gmail.com>
2023-02-20 11:44:29 -0800

    validation int main

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 /**
  *main - Entry point
  *Return Alawyas 0 (success)
@@ -16,13 +16,13 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-       	else if (n % 10 == 0)
+	else if (n % 10 == 0)
 	{
 		printf("last digit of %d is %d and is string 0\n", n, n % 10);
 	}
-	else (n % 10 < 0)
+	else
 	{
- 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
