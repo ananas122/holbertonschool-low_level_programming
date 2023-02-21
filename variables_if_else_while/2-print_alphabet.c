c480240c6d494aa1801f2c0bdc69dcff005c7a06
ananas122 <melle.noraa@gmail.com>
2023-02-21 01:38:07 -0800

    ajout .c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-#include
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - fonction principl
+ *
+ *Return: 0
+ */
 
+int main(void)
+{
+	char a;
+
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
 
 
