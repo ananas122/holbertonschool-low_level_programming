c254e0937ae13a299568c3058b21966937dc7557
ananas122 <melle.noraa@gmail.com>
2023-02-21 06:28:27 -0800

    exepted letters e q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c ='a'; c <= 'z'; c++)
+	{
+		if(c == 'e' || c == 'q')
+		{
+			c++;
+		}	
+		putchar (c);
+	}
+	putchar('\n');
+
+	return (0);	
+}
+
