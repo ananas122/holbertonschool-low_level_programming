abca23983d6c3c2457757c5ea0ec4baf323fa757
ananas122 <melle.noraa@gmail.com>
2023-02-21 08:20:30 -0800

    inverser

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,13 +10,13 @@ int main(void)
 	int num;
 	char a;
 
-	for (num = '0'; num <= '9'; num++)
-	{
-		putchar(num + '0');
-	}
 	for (a = 'a'; a <= 'f'; a++)
 	{
 		putchar(a);
+	}
+	for (a = '0'; a <= '9'; num++)
+	{
+		putchar(num + '0');
 	}
 	putchar('\n');
 
