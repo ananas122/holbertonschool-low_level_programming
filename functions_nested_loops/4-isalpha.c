a14b485e68e9b7090e338129a7f80e16b3830605
ananas122 <melle.noraa@gmail.com>
2023-02-22 08:06:57 -0800

    lettre miniscule et majuscule

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - letter, lowercase or uppercase
+ * @c: input character
+ * Return: 1 if its a letter , uppercases, lowercases or 0 if is otherwise
+ */
+int _isalpha(int c)
+{
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+		else
+		{
+			return (0);
+		}
+}
