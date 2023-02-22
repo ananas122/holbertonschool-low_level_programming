f412644b1773b90fafd5abd425e028acd3605a34
ananas122 <melle.noraa@gmail.com>
2023-02-22 12:46:23 -0800

    ajout d espace ligne 10

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,17 @@
 /**
  * print_last_digit - write a script that prints the last digit of number
  * @n: number
- *
  * Return: the value of the last digit
  */
-int print_last_digit(int num)
+int print_last_digit(int n)
 {
-	int digit = num % 10;
-	
+	int digit = n % 10;
 	if (digit < 0)
+
 	{
 	digit *= -1;
 	}
+
 _putchar(digit + '0');
 return (digit);
 }
