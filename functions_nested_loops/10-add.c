c537d78f725413d21bf9761940573dc1da219d99
ananas122 <melle.noraa@gmail.com>
2023-02-22 14:17:51 -0800

    add resultat

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the resultat
+ *@one: first number add
+ *@one: second number add
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (int n1 + int n2);
+}	
