511cc0bc165a4bad2da72a8f2716ae5aa8f25649
ananas122 <melle.noraa@gmail.com>
2023-02-22 09:11:50 -0800

    absolu

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - estimates absolu
+ * @n: integer
+ * Return: 0
+ */
+int _abs(int n)
+{
+		if (n < 0)
+		{
+		return (-n);
+		}
+		else
+		{
+		return (n);
+		}
+}
