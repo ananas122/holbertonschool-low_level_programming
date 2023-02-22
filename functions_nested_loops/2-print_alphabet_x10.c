c1f67002ad253414d219a98790dc8b9ea22211f0
ananas122 <melle.noraa@gmail.com>
2023-02-22 04:59:07 -0800

    ajout des 2 fichiers

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) - entry
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int loop;
+
+	for (loop = '1'; loop <= '10'; loop++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{	_putchar(a);
+		
+		}
+
+		_putchar('\n');
+
+		}
+
+
+
+
+
+}
