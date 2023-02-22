ea56b5f75e6a30056117ec10cc6900caf5c81c7f
ananas122 <melle.noraa@gmail.com>
2023-02-22 01:54:47 -0800

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	for ( a = 'a'; a <= 'z'; a++)
+	{		
+		_putchar(a);
+
+	}
+	_putchar('\n');
+
+
+}
