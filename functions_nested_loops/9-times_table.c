63eebd449ae5841026b88c9a17ab2b52449f1a80
ananas122 <melle.noraa@gmail.com>
2023-02-23 13:27:50 -0800

    erreurs visible sur le checker

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *times_table - afunction that prints the 9 times tables, starting 0
+ * Return : times tables
+ */
+void times_tables(void)
+
+{
+	int rone, cone, res;
+	for ( rone = 0; cone = 1; rone <= 9; cone <= 9; rone++,cone++)
+
+		res(rone*cone);
+
+	if ((res / 10) > 0)
+	{
+			_putchar ((res / 10)+ '0');
+	else 
+	{ _putchar('');
+		{_putchar((res % 10) + '0');
+			if ((cone < 9)+ '0');
+			_putchar(,);
+			_putchar('')
+		}
+	}
+		_putchar('\n');
+	}
+
