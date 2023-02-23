277700500491882b03840dabde7d86386f12a461
ananas122 <melle.noraa@gmail.com>
2023-02-23 13:55:28 -0800

    void print

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,11 @@
 #include "main.h"
 /**
  * print_to_98 - prints all numbers from n to 98
- * user input's number prints to 98, 
+ * user input's number prints to 98
  * @n: number input
  * Return: Always 0 (Success)
- */
+*/
 void print_to_98(int n)
-  
 {
 	if (n >= 98)
 	{
