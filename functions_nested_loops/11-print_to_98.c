31fb9ddc27fd1aff1120e3062fb414adcace0ea5
ananas122 <melle.noraa@gmail.com>
2023-02-23 13:45:35 -0800

    first commit

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: number starting 
+ *
+ * Return 0.
+ */
+void print_to_98(int n)
+{
+
+
+
+
+
+
+
+
+}
