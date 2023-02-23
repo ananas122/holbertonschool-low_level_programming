c82d303f84533f9fbfe5f73f1dcd08ed4ede9c41
ananas122 <melle.noraa@gmail.com>
2023-02-23 14:31:47 -0800

    ajout d espace

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,15 @@
  * Return: the value of the last digit
  */
 int print_last_digit(int n)
-{
-	int digit = n % 10;
-	if (digit < 0)
 
+{
+
+	int digit = n % 10;
+
+	if (digit < 0)
 	{
 	digit *= -1;
 	}
-
-_putchar(digit + '0');
-return (digit);
+	_putchar(digit + '0');
+	return (digit);
 }
