beb78acbf6b958927e462d0cbd831fff68bd8462
ananas122 <melle.noraa@gmail.com>
2023-02-23 14:26:53 -0800

    ajout d espace

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - adds two numbers.
  * @a: The first number to be added.
  * @b: The second number to be added.
- * Return: the sum of two numbers.
+ * Return: the sum of two numbers
  */
 int add(int a, int b)
 {
