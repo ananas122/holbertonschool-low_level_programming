5d622b8251a40cc830c38388a3dfcc3fb8175d9f
ananas122 <melle.noraa@gmail.com>
2023-02-24 07:33:35 -0800

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_square - main fun
+ *@size: numer
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+		_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
