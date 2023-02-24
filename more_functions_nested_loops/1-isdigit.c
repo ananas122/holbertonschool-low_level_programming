4991b8691b7d4f2b7fad25903a5446f147f9443f
ananas122 <melle.noraa@gmail.com>
2023-02-24 00:22:49 -0800

    ajout ascii

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isdigit - check digit 0 9
- * @c: character 0 9
+ * _isdigit - check digit 0 through 9
+ * @c: character ascii
  * Return : 1 if isdigit , else otherwise 0
  */
 
 int _isdigit(int c)
 {
-	if (c > 0 && c < 9)
+	if (c > 47 && c < 58)
 		return (1);
 
 	else
