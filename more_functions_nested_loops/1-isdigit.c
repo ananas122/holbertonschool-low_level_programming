413538e360b2a0ba8db7ef46fb78f5000f7a4020
ananas122 <melle.noraa@gmail.com>
2023-02-24 08:19:26 -0800

    ajout parametre

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,8 @@
 #include <ctype.h>
 #include "main.h"
 /**
-* _isdigit - check for a digit o through 9.  
+* _isdigit - check for a digit o through 9
+* * @c: N/A
 * Return: 1 or 0 (Success)
 */
 int _isdigit(int c)
