57dd8e78b8213e3107f566f095c65c4e69a271e4
ananas122 <melle.noraa@gmail.com>
2023-02-27 07:50:22 -0800

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
+ * Return: void
+ */
+void print_rev(char *s);
+{
+	int c = 0;
+
+	if (*s != '\0')
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(str[c]);
+	}
+
+	_putchar('\n');
+}
+
