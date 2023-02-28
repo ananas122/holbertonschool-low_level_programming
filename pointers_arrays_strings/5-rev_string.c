c347cb05960ba3dc60c69bf46a8ab55f3a0638f7
ananas122 <melle.noraa@gmail.com>
2023-02-28 06:52:04 -0800

    betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,22 @@
-#include "main.h"	
+#include "main.h"
 /**
  * rev_string - Reverses a string two words
- * @s: The string 
+ * @s: The string
  * Return: void
  */
-	
+
 void rev_string(char *s)
 {
 	int leng = 0, l = 0;
 	char temp;
-	while(s[l++])
+
+	while (s[l++])
+
 	{
 		leng++;
 	}
 
-	for ( l = leng - 1 ; l >= leng / 2; l--)
+	for (l = leng - 1 ; l >= leng / 2; l--)
 	{
 		temp = s[l];
 		s[l] = s[leng - l - 1];
