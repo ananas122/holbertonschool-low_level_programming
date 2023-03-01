b9ac01ec048bbadaad687a590087a2a1c07fa02d
ananas122 <melle.noraa@gmail.com>
2023-02-28 22:34:47 -0800

    betty espace

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
-  * _strcpy - Copy a string 
+  * _strcpy - Copy a string
   * @dest: Destination value
   * @src: Source value
   * Return: * dest
   */
 char *_strcpy(char *dest, char *src)
+
 {
 	int i;
 
