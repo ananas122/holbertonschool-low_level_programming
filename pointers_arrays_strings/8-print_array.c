78d19a7962f034c262a23cd25b2d3ef021eb9fb1
ananas122 <melle.noraa@gmail.com>
2023-03-01 07:23:41 -0800

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints  arrays 
+ * @a: an integer pointer 
+ * @n: an integer, the number of array 
+ */
+void print_array(int *a, int n)
+{
+  
+   	int i;
+
+   	for (i = 0; i < n; i++)
+	{
+      	printf("%d ", *array[i]);
+      		
+	if (i != n - 1)
+		
+	printf(", ");
+   	}
+	printf("\n");
+}  	
+
