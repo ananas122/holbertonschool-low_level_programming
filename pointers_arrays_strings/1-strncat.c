cde306d49b5186a1c0d022f32da9c454649a06dc
ananas122 <melle.noraa@gmail.com>
2023-03-02 07:19:31 -0800

    test

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - function similar strcat
+ *@s: src 
+ * Return: Always: dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+	char 
+
