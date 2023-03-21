9e486aa21504f68eeaa734566ef1f5485917bf7a
ananas122 <melle.noraa@gmail.com>
2023-03-21 06:11:59 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function given as a prmtr on each element of an array
+ * @array: pointer to array
+ * @size: size of the array
+ * @action: pointer to function you need to use
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action) (int)) 
+{
+	unsigned int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
