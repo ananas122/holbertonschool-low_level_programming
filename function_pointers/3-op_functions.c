a5e950bf7e93025585e16f9042647b3e34f70f0d
ananas122 <melle.noraa@gmail.com>
2023-03-22 04:34:42 -0700

    add files task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds 5 functions
+ * @a: int a
+ * @b: int b
+ * Return: sum 
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies
+ * @a: int a
+ * @b: int b
+ * Return: multipliy
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: int a
+ * @b: int b
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos
+ * @a: int a
+ * @b: int b
+ * Return: modulo of ints
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
