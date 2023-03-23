64ff5993b1e8991a8eb77385448a7b0a6d572764
ananas122 <melle.noraa@gmail.com>
2023-03-23 06:52:34 -0700

    sums

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Entry Point
+ * @n: variadic inputs
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+	unsigned int i;
+       	int sum = 0;
+	
+	va_list arg;
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
+
+	return (sum);
+}
