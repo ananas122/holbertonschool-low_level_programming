fa3495f829bbcfec9e9359632e4d0c838010f421
ananas122 <melle.noraa@gmail.com>
2023-03-24 07:42:43 -0700

    print all and variadic function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct format - Struct format
+ *
+ * @format: the string format
+ * @func: the function for which way to print
+ */
+typedef struct format
+{
+	char *format;
+	void (*func)(va_list);
+} format_type;
 
 #endif
