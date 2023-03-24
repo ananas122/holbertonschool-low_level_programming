a386098d2481519cf3da20521bd341f700534033
ananas122 <melle.noraa@gmail.com>
2023-03-24 02:57:43 -0700

    task 1 betty espace

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,21 +10,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list arguments; 
+	va_list arguments;
 
 	va_start(arguments, n);
 
 
 	for (i = 0; i < n; i++)
 	{
-	
+
 		printf("%d", va_arg(arguments, int));
-	
+
 		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
 	}
 	printf("\n");
-	va_end(arguments); 
+	va_end(arguments);
 }
