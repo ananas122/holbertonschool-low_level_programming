0109bdaab6f26136b4d8cf07298a1f76de2314d4
ananas122 <melle.noraagmail.com>
2023-04-04 01:38:44 -0700

    ajout fichier lists.h et task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+  * print_list - prints the contentsof a list_t list
+  * @h: pointer
+  * Return: numbers of nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+    size_t nodes = 0;       /* Initialisation du compteur à zéro*/
+
+    /* Boucle pour parcourir tous les nœuds de la liste*/
+	while (h != NULL) 
+	{
+        	if (h->str == NULL)
+		/*Si la chaîne est NULL, imprimer [0] (nil)*/
+	 		printf("[0] (nil)\n");
+
+		else 
+            /* Sinon, imprimer la chaîne et sa position dans la liste*/
+            		printf("[%u] %s\n", h->len, h->str);
+        
+        	nodes++;            /*Incrémenter le compteur*/
+        	h = h->next;        /* Avancer au prochain nœud*/
+	}	
+	return (nodes);           /*Retourner le nombre total de nœuds dans la liste*/
+}
+
