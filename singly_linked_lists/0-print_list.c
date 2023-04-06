8e3932d4d3327635f32aa073ce0556763670c1dc
ananas122 <melle.noraagmail.com>
2023-04-06 00:56:37 -0700

    task 0 betty correction

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,22 +9,22 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t nodes = 0;       /* Initialisation du compteur à zéro*/
+	size_t nodes = 0;       /* Initialisation du compteur à zéro*/
 
-    /* Boucle pour parcourir tous les nœuds de la liste*/
-	while (h != NULL) 
+	/* Boucle pour parcourir tous les nœuds de la liste*/
+	while (h != NULL)
 	{
-        	if (h->str == NULL)
+		if (h->str == NULL)
 		/*Si la chaîne est NULL, imprimer [0] (nil)*/
-	 		printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 
-		else 
-            /* Sinon, imprimer la chaîne et sa position dans la liste*/
-            		printf("[%u] %s\n", h->len, h->str);
-        
-        	nodes++;            /*Incrémenter le compteur*/
-        	h = h->next;        /* Avancer au prochain nœud*/
-	}	
-	return (nodes);           /*Retourner le nombre total de nœuds dans la liste*/
+		else
+		/* Sinon, imprimer la chaîne et sa position dans la liste*/
+		printf("[%u] %s\n", h->len, h->str);
+
+		nodes++;            /*Incrémenter le compteur*/
+		h = h->next;        /* Avancer au prochain nœud*/
+	}
+	return (nodes);
+	/*Retourner le nombre total de nœuds dans la liste*/
 }
-
