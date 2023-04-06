993a0e0b40c6aa996b6ed31a0a9e17b71a1388e0
ananas122 <melle.noraagmail.com>
2023-04-06 01:05:45 -0700

    correction betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
 #include "lists.h"
 #include <stdio.h>
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 
-/* Fonction pour libérer la mémoire allouée pour tous les nœuds de la liste*/
+/*Fonction pr libérer la mémoire allouée pr ts les nœuds de la liste*/
 void free_list(list_t *head)
 {
-    list_t *temp;           /* Pointeur temporaire pour stocker l'adresse du nœud suivant*/
+	list_t *temp;
+	/* Ptr temp pr stocker l'adresse du nœud suivant*/
 
-    /*Boucle pour parcourir tous les nœuds de la liste*/
-    while (head != NULL) 
-    {
-        temp = head->next;  /* Stocker l'adresse du nœud suivant*/
-        free(head->str);    /* Libérer la mémoire allouée pour la chaîne de caractères*/
-        free(head);         /* Libérer la mémoire allouée pour le nœud*/
-        head = temp;        /* Avancer au nœud suivant*/
-    }
+	/*Boucle pour parcourir tous les nœuds de la liste*/
+	while (head != NULL)
+	{
+	temp = head->next;  /* Stocker l'adresse du nœud suivant*/
+	free(head->str);    /* Libérer la mém allouée pr la chaîne de krc*/
+	free(head);         /* Libérer la mémoire allouée pour le nœud*/
+	head = temp;        /* Avancer au nœud suivant*/
+	}
 }
-
