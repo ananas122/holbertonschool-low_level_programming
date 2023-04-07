5ef2473b97dd6b3111949ce2d43da67f890aa0a2
ananas122 <melle.noraagmail.com>
2023-04-07 04:23:25 -0700

    task 6 sum list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all the ints in list
+ * @head: head of the list
+ * Return: sum or 0 if empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	/* initialisation */
+	int sum = 0;
+
+	/* if list is empty */
+	if (!head)
+		return (0);
+	/* otherwise, add them up */
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
+
