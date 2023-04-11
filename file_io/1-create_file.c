60b7fc8b5500df4cfbe64d1d89016c8f865b80a4
ananas122 <melle.noraagmail.com>
2023-04-11 02:23:06 -0700

    ajout prmtr

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,6 +2,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text
+ * Return: 1 - success, -1 error
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int o;        /* descripteur de fichier*/
@@ -21,6 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		int i = 0;
+
 		while (text_content[i] != '\0')
 			i++;
 		w = write(o, text_content, i);
