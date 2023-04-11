aa749394f2e5e2b4d3758e45d2d213b40358b961
ananas122 <melle.noraagmail.com>
2023-04-11 07:16:25 -0700

    ajout des parametres

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * create_file - creates a file
  * @filename: name of file
@@ -11,35 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o;        /* descripteur de fichier*/
-	ssize_t w;    /*Résultat de la fonction write*/
+	int file, num;
 
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	/* O_WRONLY : Ouverture en mode écriture seulement*/
 	/* O_CREAT : Création du fichier s'il n'existe pas*/
 	/* O_TRUNC : Tronquer le fichier si déjà existant*/
 	/* 0600 : Permissions rw-------*/
-	if (o == -1)
+
+	if (file == -1)
 		return (-1);
-
-	if (text_content != NULL)
+	if (text_content)
 	{
-		int i = 0;
-
-		while (text_content[i] != '\0')
-			i++;
-		w = write(o, text_content, i);
-		if (w == -1)
-		{
-			close(o);
+		num = write(file, text_content, strlen(text_content));
+		if (num == -1)
 			return (-1);
-		}
 	}
-
-	close(o);
 	return (1);
 }
-
