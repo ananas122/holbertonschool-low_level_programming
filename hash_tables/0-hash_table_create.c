158aa575cb2d26fb9b6748979fa09dcc613a073e
ananas122 <melle.noraagmail.com>
2023-04-13 03:04:48 -0700

    correction betty espace

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
+ * hash_table_create - creates hash table
  * @size: size of the array
  * Return: pointer to hash table, otherwise return NULL
  */
