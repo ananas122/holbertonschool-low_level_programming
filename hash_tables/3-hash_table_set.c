e0c7f648b69b9fa6bbaab2d64cf8c4dce5ee12c0
ananas122 <melle.noraagmail.com>
2023-04-14 02:17:03 -0700

    set task 4 betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long index, size;
 hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0') 
+	if (ht == NULL || key == NULL || *key == '\0')
 	/* Check for NULL hash table or empty key*/
 		return (0);
 
